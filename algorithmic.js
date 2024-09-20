0fd99e6d8ff2d7e17c27383049bdc03cbae07147
meatbroc <168386225+meatbroc@users.noreply.github.com>
2024-09-20 18:02:25 -0400

    Update algorithmic.js

--- algorithmic.js
+++ algorithmic.js
@@ -1,31 +1,41 @@
-let x;
-
-x.set = function (c) {
-  if (typeof c == 'string') {
-    let d = c.toUpperCase()
-    return d
+let x = {
+  set: function (c) {
+    if (typeof c == 'string') {
+      let d = c.toUpperCase();
+      return d;
+    }
   }
-}
+};
 
-x.fetchAirports = async function getData() {
+let airport1 = 'KSFO'
+let airport2 = 'RJTT'
+
+let fetchAirports = async function getData() {
   const url = "https://www.geo-fs.com/data/airports.js";
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-    x = {};
-    x.a = 'RJTT'
-    x.b = 'KSFO'
-    x.text = await response.text();
-    x.text = eval(x.text)
+    const internalText = await response.text();
+    return eval(internalText); // Be cautious with eval
   } catch (error) {
     console.error(JSON.stringify(error.message));
   }
 };
 
+async function main() {
+  x.text = await fetchAirports();
+
+  const coord1 = { lat: x.text[airport1][0], lon: x.text[airport1][1] };
+  const coord2 = { lat: x.text[airport2][0], lon: x.text[airport2][1] };
+
+  const distance = haversineDistance(coord1, coord2);
+  console.log(`Distance: ${distance} miles`);
+}
+
 function toRadians(degrees) {
-    return degrees * (Math.PI / 180);
+  return degrees * (Math.PI / 180);
 }
 
 function haversineDistance(coord1, coord2) {
@@ -46,11 +56,8 @@ function haversineDistance(coord1, coord2) {
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     const distance = R * c;
-    return distance;
+    const miles = distance * 0.621371
+    return miles;
 }
 
-const coord1 = { lat: x.set(x.a)[0], lon: x.set(x.a)[1] }
-const coord2 = { lat: x.set(x.b)[0], lon: x.set(x.b)[1] };
-
-const distance = haversineDistance(coord1, coord2);
-console.log(`Distance: ${distance} km`);
+main();
